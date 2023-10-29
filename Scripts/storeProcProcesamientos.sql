8d5d6ced5fe591a23d29781bcad717c5dc2e939f
unknown <3236875710511@ingenieria.usac.edu.gt>
2023-10-29 13:28:46 -0600

    Consultar estudiante

--- Scripts/storeProcProcesamientos.sql
+++ Scripts/storeProcProcesamientos.sql
@@ -20,3 +20,25 @@ proc_pensum: BEGIN
 	SELECT codigo, nombre, obligatorio, crd_nec FROM curso WHERE curso.id_carrera = id_pensum OR curso.id_carrera = 0;
 END $$
 DELIMITER ;
+
+DROP PROCEDURE IF EXISTS consultarEstudiante;
+DELIMITER $$
+CREATE PROCEDURE consultarEstudiante(
+IN carnt BIGINT
+)
+proc_estudiante_cons: BEGIN
+	DECLARE result BOOLEAN;
+    IF carnt IS NULL THEN
+		CALL Mensaje("Error: Debes introducir el número de carnet del estudiante a consultar");
+        LEAVE proc_estudiante_cons;
+	ELSE 
+		SELECT EXISTS(SELECT carnet FROM estudiante WHERE estudiante.carnet = carnt) INTO result;
+        IF NOT result THEN
+			CALL Mensaje("Error: Estudiante no encontrado, verifica el número de carnet");
+            LEAVE proc_estudiante_cons;
+		END IF;
+	END IF;
+    SELECT carnet, CONCAT(nombres,' ',apellidos) as nombre_completo, fecha_nacimiento, correo, telefono, direccion, dpi, id_carrera, creditos 
+    FROM estudiante WHERE estudiante.carnet = carnt;
+END $$
+DELIMITER ;
