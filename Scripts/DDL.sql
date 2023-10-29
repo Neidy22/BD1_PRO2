e7fb74a00d0942f3ef5e58a37e8ddf1f2abfcb5f
unknown <3236875710511@ingenieria.usac.edu.gt>
2023-10-29 10:53:46 -0600

    Crear carrera y registrar estudiante

---
+++ Scripts/DDL.sql
@@ -0,0 +1,126 @@
+use pro2;
+CREATE TABLE IF NOT EXISTS  carrera(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS estudiante(
+	carnet BIGINT PRIMARY KEY,
+    nombres VARCHAR(50) NOT NULL,
+    apellidos VARCHAR(50) NOT NULL,
+    fecha_nacimiento DATE NOT NULL,
+    correo VARCHAR(100) NOT NULL,
+    telefono INT NOT NULL,
+    direccion VARCHAR(100) NOT NULL,
+    dpi BIGINT NOT NULL,
+    id_carrera INT NOT NULL, 
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    creditos INT NOT NULL,
+    FOREIGN KEY (id_carrera) REFERENCES carrera(id)
+);
+
+CREATE TABLE IF NOT EXISTS curso(
+	codigo INT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    crd_nec INT NOT NULL,
+    crd_otg INT NOT NULL, 
+    obligatorio BOOLEAN NOT NULL, 
+    id_carrera INT NOT NULL,
+    FOREIGN KEY (id_carrera) REFERENCES carrera(id)
+);
+
+CREATE TABLE IF NOT EXISTS docente(
+	siif INT PRIMARY KEY,
+    nombres VARCHAR(50) NOT NULL,
+    apellidos VARCHAR(50) NOT NULL,
+    fecha_nacimiento DATE NOT NULL,
+    correo VARCHAR(100) NOT NULL,
+    telefono INT NOT NULL,
+    direccion VARCHAR(100) NOT NULL,
+    dpi BIGINT NOT NULL,
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS curso_habilitado(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    ciclo VARCHAR(2) NOT NULL,
+    cupo_max INT NOT NULL,
+    seccion CHAR NOT NULL,
+    id_docente INT NOT NULL,
+    id_curso INT NOT NULL,
+    anio INT NOT NULL, 
+    FOREIGN KEY (id_docente) REFERENCES docente(siif),
+    FOREIGN KEY (id_curso) REFERENCES curso(codigo)
+);
+
+CREATE TABLE IF NOT EXISTS horario(
+	dia INT NOT NULL,
+    rango VARCHAR(12) NOT NULL,
+    id_curso INT NOT NULL,
+    PRIMARY KEY (dia, id_curso),
+    FOREIGN KEY (id_curso) REFERENCES curso_habilitado(id)
+);
+
+CREATE TABLE IF NOT EXISTS asignacion(
+	id INT PRIMARY KEY,
+    id_curso INT NOT NULL,
+    ciclo VARCHAR(2),
+    seccion CHAR(1),
+    FOREIGN KEY (id_curso) REFERENCES curso(codigo)
+);
+CREATE TABLE IF NOT EXISTS desasignacion(
+	id INT PRIMARY KEY,
+    id_curso INT NOT NULL,
+    ciclo VARCHAR(2),
+    seccion CHAR(1),
+    FOREIGN KEY (id_curso) REFERENCES curso(codigo)
+);
+
+CREATE TABLE IF NOT EXISTS nota(
+	id INT PRIMARY KEY,
+    id_curso INT NOT NULL,
+    ciclo VARCHAR(2),
+    seccion CHAR(1),
+    anio INT NOT NULL,
+    FOREIGN KEY (id_curso) REFERENCES curso(codigo)
+);
+
+CREATE TABLE IF NOT EXISTS detalle_asignacion(
+	id INT PRIMARY KEY,
+    carnet BIGINT NOT NULL, 
+    id_asign INT NOT NULL,
+    FOREIGN KEY (carnet) REFERENCES estudiante(carnet),
+    FOREIGN KEY (id_asign) REFERENCES asignacion(id)
+);
+
+CREATE TABLE IF NOT EXISTS detalle_desasignacion(
+	id INT PRIMARY KEY,
+    carnet BIGINT NOT NULL, 
+    id_desasign INT NOT NULL,
+    FOREIGN KEY (carnet) REFERENCES estudiante(carnet),
+    FOREIGN KEY (id_desasign) REFERENCES desasignacion(id)
+);
+
+CREATE TABLE IF NOT EXISTS detalle_nota(
+	id INT PRIMARY KEY,
+    nota INT NOT NULL, 
+    carnet BIGINT NOT NULL,
+    id_nota INT NOT NULL,
+    FOREIGN KEY (carnet) REFERENCES estudiante(carnet),
+    FOREIGN KEY (id_nota) REFERENCES nota(id)
+);
+
+CREATE TABLE IF NOT EXISTS acta(
+	id INT PRIMARY KEY,
+    id_curso INT NOT NULL,
+    ciclo VARCHAR(2) NOT NULL,
+    seccion CHAR NOT NULL,
+    FOREIGN KEY (id_curso) REFERENCES curso(codigo)
+);
+
+CREATE TABLE IF NOT EXISTS historial(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    fecha_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    descripcion VARCHAR(200) NOT NULL,
+    tipo VARCHAR(20) NOT NULL
+);
---
+++ Scripts/storeProc.sql
@@ -0,0 +1,172 @@
+use pro2;
+
+DROP PROCEDURE IF EXISTS crearCarrera;
+DELIMITER $$
+CREATE PROCEDURE crearCarrera(
+-- params
+IN nombre varchar(50)
+)
+proc_carrera: BEGIN
+	DECLARE result BOOLEAN;
+    IF nombre IS NULL THEN
+		CALL Mensaje("Error, debe ingresar nombre de carrera obligatoriamente");
+        LEAVE proc_carrera;
+	ELSE
+		SET result = validarLetras(nombre);
+        IF NOT result THEN
+			CALL Mensaje("Error, el nombre de carrera únicamente puede contener letras");
+			LEAVE proc_carrera;
+		END IF;
+	END IF;
+    INSERT INTO carrera(nombre) 
+    VALUES(nombre);
+    CALL Mensaje("Carrera registrada con éxito");
+END $$
+DELIMITER ;
+
+
+DROP PROCEDURE IF EXISTS registrarEstudiante;
+delimiter $$
+CREATE PROCEDURE registrarEstudiante(
+-- parametros
+IN carnet bigint,
+IN nombres varchar(45),
+IN apellidos varchar(45),
+IN fechanac varchar(10),
+IN email varchar(45),
+IN tel int,
+IN direccion varchar(45),
+IN dpi bigint,
+IN carrera int
+)
+
+proc_estudiante: BEGIN
+-- instruciones
+	DECLARE fech DATE;
+    DECLARE fechan DATE;
+    DECLARE result BOOLEAN;
+    DECLARE id_c INT;
+    SET  fechan = STR_TO_DATE(fechanac, '%d-%m-%Y');
+    -- validar carnet
+    IF carnet  IS NULL THEN 
+		CALL Mensaje("Error, debe ingresar carnet obligatoriamente");
+        LEAVE proc_estudiante;
+	ELSE
+		SET result = validarNumeros(carnet);
+		IF NOT result THEN
+			CALL Mensaje("Error, el carnet solo puede contener números");
+			LEAVE proc_estudiante;
+		END IF;
+	END IF;
+    
+    -- validar nombres
+	IF nombres IS NULL THEN 
+		CALL Mensaje("Error, debe ingresar nombres obligatoriamente");
+        LEAVE proc_estudiante; 
+	ELSE 
+		SET result = validarLetras(nombres);
+        IF NOT result THEN
+			CALL Mensaje("Error, los nombres solo puede contener letras");
+			LEAVE proc_estudiante;
+		END IF;
+	END IF;
+        
+	-- validar apellidos
+	IF apellidos IS NULL THEN 
+		CALL Mensaje("Error, debe ingresar los apellidos obligatoriamente");
+        LEAVE proc_estudiante; 
+	ELSE 
+		SET result = validarLetras(nombres);
+        IF NOT result THEN
+			CALL Mensaje("Error, los apellidos solo puede contener letras");
+			LEAVE proc_estudiante;
+		END IF;
+	END IF;
+        
+    -- validar fecha  nacimiento
+	IF fechanac IS NULL THEN 
+		CALL Mensaje("Error, debe ingresar fecha de nacimiento obligatoriamente");
+        LEAVE proc_estudiante; 
+	END IF;
+    
+	-- validar email
+	IF email IS NULL THEN 
+		CALL Mensaje("Error, debe ingresar correo obligatoriamente");
+        LEAVE proc_estudiante; 
+	ELSE
+		SET result = validarEmail(email);
+        IF NOT result THEN
+			CALL Mensaje("Error, el formato del correo debe ser el siguiente usuario@organizacion.tipo");
+			LEAVE proc_estudiante; 
+		END IF;
+	END IF;
+	
+    -- validar direccion
+	IF direccion IS NULL THEN 
+		CALL Mensaje("Error, debe ingresar direccion obligatoriamente");
+        LEAVE proc_estudiante;
+	END IF;
+    
+	-- validar dpi
+	IF dpi IS NULL THEN 
+		CALL Mensaje("Error, debe ingresar dpi obligatoriamente");
+        LEAVE proc_estudiante; 
+	ELSE 
+		SET result = validarNumeros(dpi);
+        IF NOT result THEN
+			CALL Mensaje("Error, el dpi solo puede contener números");
+			LEAVE proc_estudiante; 
+		END IF;
+	END IF;
+    
+	-- validar carrera
+	IF carrera IS NULL THEN 
+		CALL Mensaje("Error, debe ingresar carrera obligatoriamente");
+        LEAVE proc_estudiante; 
+	ELSE 
+		SELECT EXISTS(SELECT id FROM carrera WHERE carrera.id = carrera) INTO result;
+        IF NOT result THEN
+			CALL Mensaje("Error, la carrera no existe");
+			LEAVE proc_estudiante; 
+        END IF;
+	END IF;
+    
+    SET fech = curdate();
+    INSERT INTO estudiante(carnet, nombres, apellidos, fecha_nacimiento, correo, telefono, direccion, dpi, id_carrera, creditos)
+    VALUES (carnet,nombres,apellidos,fechan,email,tel,direccion,dpi,carrera,0);
+    CALL Mensaje("Estudiante Registrado correctamente");
+END $$
+delimiter ;
+
+
+DROP PROCEDURE IF EXISTS Mensaje;
+delimiter $$ 
+CREATE PROCEDURE Mensaje(
+IN msg varchar(200)
+)
+BEGIN 
+	SELECT msg as Result;
+END $$
+delimiter ;
+
+DROP PROCEDURE IF EXISTS dropData;
+delimiter $$ 
+CREATE PROCEDURE dropData(
+)
+BEGIN 
+    DROP TABLE IF EXISTS detalle_asignacion;
+    DROP TABLE IF EXISTS detalle_desasignacion;
+    DROP TABLE IF EXISTS detalle_nota;
+    DROP TABLE IF EXISTS asignacion;
+    DROP TABLE IF EXISTS desasignacion;
+    DROP TABLE IF EXISTS nota;
+    DROP TABLE IF EXISTS acta;
+    DROP TABLE IF EXISTS horario;
+    DROP TABLE IF EXISTS curso_habilitado;
+    DROP TABLE IF EXISTS estudiante;
+    DROP TABLE IF EXISTS curso;
+    DROP TABLE IF EXISTS historial;
+    DROP TABLE IF EXISTS carrera;
+    DROP TABLE IF EXISTS docente;
+END $$
+delimiter ;
